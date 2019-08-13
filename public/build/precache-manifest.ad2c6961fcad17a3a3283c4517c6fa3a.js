79d45410443c0fab4b1b002c34d114b49d93f3af
gerson-evocorp <gerson@evocorp.com.br>
2019-08-13 18:18:36 -0300

    build form adiconada

---
+++ public/build/precache-manifest.ad2c6961fcad17a3a3283c4517c6fa3a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1c9635fc61fd9cf8c335e96b1c3d8d9",
+    "url": "/static/media/logo.b1c9635f.png"
+  },
+  {
+    "revision": "b70493c35f41459416da0dc2062cb09d",
+    "url": "/static/media/banner2.b70493c3.jpg"
+  },
+  {
+    "revision": "e6fc4790cfc4ba6c9a351257c31fa0aa",
+    "url": "/static/media/banner.e6fc4790.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8cdf7a1592e2ab4270c4",
+    "url": "/static/js/main.96a72d0e.chunk.js"
+  },
+  {
+    "revision": "5fa35df961c22262b3e2",
+    "url": "/static/js/2.4c6b1fa3.chunk.js"
+  },
+  {
+    "revision": "8cdf7a1592e2ab4270c4",
+    "url": "/static/css/main.8d305844.chunk.css"
+  },
+  {
+    "revision": "5fa35df961c22262b3e2",
+    "url": "/static/css/2.c4e2e5f1.chunk.css"
+  },
+  {
+    "revision": "1bd42b1651cb21c83a811382d7caef3d",
+    "url": "/index.html"
+  }
+];
